f3ef23031d7283dfdcf9fcf766be3e5142fa83fc
Demi Lune <anshikathakur596@gmail.com>
2024-11-28 12:18:40 +0530

    added gitignore

--- src/com/virtualpet/pet/PetType.java
+++ src/com/virtualpet/pet/PetType.java
@@ -1,5 +1,45 @@
 package com.virtualpet.pet;
 
-public class PetType {
+import java.util.Scanner;
 
+public enum PetType {
+    DOG,
+    CAT,
+    BIRD;
+
+    // Method to prompt user to select a pet
+    public static PetType choosePet() {
+        Scanner scanner = new Scanner(System.in);
+        PetType selectedPet = null;
+
+        String PetTypeTxt = """
+                Choose a pet to start with:
+                1. Dog
+                2. Cat
+                3. Bird
+                Enter your choice (1/2/3):
+                """;    
+
+        // Display pet options
+        System.out.println(PetTypeTxt);
+        
+        // Validate user input
+        try {
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1 -> selectedPet = PetType.DOG;
+                case 2 -> selectedPet = PetType.CAT;
+                case 3 -> selectedPet = PetType.BIRD;
+                default -> {
+                    System.out.println("Invalid choice, defaulting to Dog.");
+                    selectedPet = PetType.DOG;
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Invalid input! Defaulting to Dog.");
+            selectedPet = PetType.DOG;
+        }
+
+        return selectedPet;
+    }
 }
