0d8689d1e937a8457675f71861b220b18639f0ed
Demi Lune <anshikathakur596@gmail.com>
2024-12-02 11:43:55 +0530

    Update PetName.java

--- src/com/virtualpet/pet/PetName.java
+++ src/com/virtualpet/pet/PetName.java
@@ -10,9 +10,20 @@ public class PetName {
         System.out.print("Enter a name: ");
         String petName = scanner.nextLine().trim();
 
-        // Validate input: Ensure it's not empty
-        while (petName.isEmpty()) {
-            System.out.println("\nName cannot be empty. Please enter a valid name:");
+        // Validate input: Ensure it's not empty, doesnt start with a number, doesnt end with a special character, doesnt start with a special character or doesnt have just one character
+        while (petName.isEmpty() || petName.matches("^[0-9].*") || petName.matches("^[^a-zA-Z0-9].*") || petName.matches(".*[^a-zA-Z0-9]$") || (petName.length() == 1)) {
+            if (petName.isEmpty()){
+                System.out.println("\nName cannot be empty. Please enter a valid name:");
+            } else if (petName.matches("^[0-9].*")) {
+                System.out.println("\nName cannot start with a number. Please enter a valid name:");
+            } else if (petName.matches("^[^a-zA-Z0-9].*")) {
+                System.out.println("\nName must start with a letter or number. Please enter a valid name:");
+            } else if (petName.matches(".*[^a-zA-Z0-9]$")) {
+                System.out.println("\nName must end with a letter or number. Please enter a valid name:");
+            } else if (petName.length() == 1) {
+                System.out.println("\nName must be longer than one character. Please enter a valid name:");
+            }
+
             System.out.print("Enter a name: ");
             petName = scanner.nextLine().trim();
         }
